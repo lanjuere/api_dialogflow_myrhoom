ebd31515aedaf5d7acc57be8064ab53b1777423c
lanjuere <lionel.anjuere@gmail.com>
2018-06-26 23:58:19 +0200

    [INIT] add serveur

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
 // for Dialogflow fulfillment library docs, samples, and to report issues
 'use strict';
- 
+
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
- 
+
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
- 
+
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
- 
+
   function welcome(agent) {
     agent.add(`Welcome to my agent!`);
   }
- 
+
   function fallback(agent) {
     agent.add(`I didn't understand`);
     agent.add(`I'm sorry, can you try again?`);
-}
+  }
 
   // // Uncomment and edit to make your own intent handler
   // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
   // // below to get this function to be run when a Dialogflow intent is matched
-//   function yourFunctionHandler(agent) {
-//     agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
-//     agent.add(new Card({
-//         title: `Title: this is a card title`,
-//         imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-//         text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-//         buttonText: 'This is a button',
-//         buttonUrl: 'https://assistant.google.com/'
-//       })
-//     );
-//     agent.add(new Suggestion(`Quick Reply`));
-//     agent.add(new Suggestion(`Suggestion`));
-//     agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-//   }
+  //   function yourFunctionHandler(agent) {
+  //     agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+  //     agent.add(new Card({
+  //         title: `Title: this is a card title`,
+  //         imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+  //         text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+  //         buttonText: 'This is a button',
+  //         buttonUrl: 'https://assistant.google.com/'
+  //       })
+  //     );
+  //     agent.add(new Suggestion(`Quick Reply`));
+  //     agent.add(new Suggestion(`Suggestion`));
+  //     agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+  //   }
 
   // // Uncomment and edit to make your own Google Assistant intent handler
   // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
@@ -58,4 +58,38 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   // intentMap.set('your intent name here', yourFunctionHandler);
   // intentMap.set('your intent name here', googleAssistantHandler);
   agent.handleRequest(intentMap);
+});
+
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+try{
+ 
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+  const agent = new WebhookClient({ request, response });
+
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+  // res.statusCode = 200;
+  // res.setHeader('Content-Type', 'text/plain');
+  // res.end('Hello World\n');
+
+}catch(err){
+console.error(err);
+}
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
